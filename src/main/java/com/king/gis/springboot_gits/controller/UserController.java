17517935470d5f9d9b1ca45cd29ee64cc7d2c04e
king <2907984560@qq.com>
2023-03-08 22:01:02 +0800

    UserController 这个是用户登录功能

--- src/main/java/com/king/gis/springboot_gits/controller/UserController.java
+++ src/main/java/com/king/gis/springboot_gits/controller/UserController.java
@@ -3,5 +3,23 @@ package com.king.gis.springboot_gits.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("你好你好  这个是最终版本 以后 不会在修改了");
+        System.out.println("你好你好  这个是最终版本 以后 不会在修改了");
+        System.out.println("你好你好  这个是最终版本 以后 不会在修改了");
+
+        System.out.println("你好你好  这个是最终版本 以后 不会在修改了");
+
+        System.out.println("你好你好  这个是最终版本 以后 不会在修改了");
+
+        System.out.println("你好你好  这个是最终版本 以后 不会在修改了");
+        System.out.println("你好你好  这个是最终版本 以后 不会在修改了");
+
+
+
+
+
+
+
+
     }
 }
